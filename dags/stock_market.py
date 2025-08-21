b563bbee2371078beaab501ffb0075fc0879f079
Ashish Shivajirao Jadhav <jashish387@gmail.com>
2025-08-21 23:10:52 +0200

    Remove MinIO system files from tracking

--- dags/stock_market.py
+++ dags/stock_market.py
@@ -1,41 +1,80 @@
+# Standard library imports
 from datetime import datetime
 
+# Third party imports
 from airflow.decorators import dag, task
 from airflow.hooks.base import BaseHook
-from airflow.sensors.base import PokeReturnValue 
 from airflow.operators.python import PythonOperator
+from airflow.providers.docker.operators.docker import DockerOperator
+from airflow.sensors.base import PokeReturnValue
 
+# Custom imports
+from include.stock_market.tasks import _get_stock_prices, _store_prices
+
+SYMBOL = "NVDA"
 
 # Define the basic parameters of the DAG, like schedule and start_date
+
+
 @dag(
     start_date=datetime(2024, 1, 1),
     schedule="@daily",
     catchup=False,
     tags=["stock_market"],
 )
-
 def stock_market():
-    
+
     # Define the tasks of the DAG
-    @task.sensor(poke_interval=30, timeout=300, mode='poke')
-    def is_api_available()->PokeReturnValue:
+    # Task 1
+    @task.sensor(task_id='is_api_available', poke_interval=30, timeout=300, mode='poke')
+    def is_api_available() -> PokeReturnValue:
         """Logic to check if the stock API is available"""
         import requests
-        api = BaseHook.get_connection("stock_api")
-        url =f"{api.host}{api.extra_dejson['endpoint']}"
+        # Access the connection setup in Airflow UI via BaseHook
+        api = BaseHook.get_connection(conn_id="stock_api")
+        url = f"{api.host}{api.extra_dejson['endpoint']}"
         print(f"Checking API availability at {url}")
+
         # Make GET request to the Weather API
         response = requests.get(url, headers=api.extra_dejson['headers'])
 
         condition = response.json()['finance']['result'] is None
         return PokeReturnValue(is_done=condition, xcom_value=url)
-    
+
+    # Task 2
     get_stock_prices = PythonOperator(
-        task_id='get_stock_prices',
-        python_callable=_get_stock_prices
+        task_id='get_stock_prices',  # Unique Task name
+        python_callable=_get_stock_prices,
+        op_kwargs={
+            'url': '{{ ti.xcom_pull(task_ids="is_api_available") }}', 'symbol': SYMBOL},  # Keyword arguments of the function
+    )
+    # Task 3
+    store_prices = PythonOperator(
+        task_id='store_prices',
+        python_callable=_store_prices,
+        op_kwargs={
+            'stock': '{{ ti.xcom_pull(task_ids="get_stock_prices") }}'},
     )
 
-    is_api_available()
+    # Task 4
+    format_prices = DockerOperator(
+        task_id='format_prices',
+        image='airflow/stock-app',
+        container_name='format_prices',
+        api_version='auto',
+        auto_remove='success',
+        docker_url='tcp://docker-proxy:2375',
+        network_mode='container:spark-master',
+        tty=True,
+        xcom_all=False,
+        environment={
+            'SPARK_APPLICATION_ARGS': '{{ ti.xcom_pull(task_ids="store_prices") }}'
+        }
+
+    )
+
+# Set sequence of the tasks
+    is_api_available() >> get_stock_prices >> store_prices >> format_prices
 
 
 # Create the DAG
---
+++ include/stock_market/tasks.py
@@ -0,0 +1,58 @@
+# Standard library imports
+from io import BytesIO
+
+# Third party imports
+from airflow.hooks.base import BaseHook
+from minio import Minio
+
+
+def _get_stock_prices(url: str, symbol: str) -> str:
+    """
+    Fetch stock prices from the API.
+    """
+    import json
+
+    import requests
+
+    url = f"{url}{symbol}?metrics=high&interval=1d&range=1y"
+
+    # Get Connection details via basehook
+    api = BaseHook.get_connection(conn_id="stock_api")
+    # Get requests.Response object
+    response = requests.get(url, headers=api.extra_dejson['headers'])
+    print(f"Type of response parameter: {type(response)}")
+    # Serialise and return the object
+    return json.dumps(response.json()['chart']['result'][0])
+
+
+def _store_prices(stock: dict) -> str:
+    """ Store the data in a MinIO  database and returns the object path  """
+    import json
+    # Get Connection details via basehook
+    minio = BaseHook.get_connection(conn_id="minio")
+
+    # Create a Minio client
+    # Add database endpoint url
+    # Add username and password of the database
+    client = Minio(
+        endpoint=minio.extra_dejson['endpoint_url'].split('//')[1],
+        access_key=minio.login,
+        secret_key=minio.password,
+        secure=False
+    )
+    # Create the bucket if it doesn't exist
+    bucket_name = 'stock-market'
+    if not client.bucket_exists(bucket_name):
+        client.make_bucket(bucket_name)
+        
+    # Store the stock prices object in the MinIO bucket
+    stock = json.loads(stock)
+    symbol = stock['meta']['symbol']
+    data = json.dumps(stock, ensure_ascii=False).encode('utf-8')  # Serialize into json string
+    # Store the file into the database
+    objw = client.put_object(bucket_name=bucket_name,
+                             object_name=f"{symbol}/prices.json",
+                             data=BytesIO(data),
+                             length=len(data)
+                             )
+    return f"{objw.bucket_name}/{symbol}"
